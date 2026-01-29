29c6bac63d2b205580a15cba2e6ff498d9e76ddf
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2026-01-29 18:17:27 +0330

    Initial commit: Vue 3 + Vuetify PDF Normalizer
    - Google Drive integration for reading PDFs from public folders
    - PDF processing with pdf-lib (standardization, compression)
    - Google OAuth for uploading processed files back to Drive
    - Vuetify 3 UI with progress tracking
    - Vercel deployment configuration

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary" density="compact">
+      <v-app-bar-title>
+        <v-icon class="mr-2">mdi-file-pdf-box</v-icon>
+        PDF Normalizer
+      </v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container class="py-8">
+        <PdfProcessor />
+      </v-container>
+    </v-main>
+
+    <v-footer app class="text-center d-flex flex-column py-3">
+      <div class="text-caption">
+        Process and normalize PDFs from Google Drive
+      </div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script setup>
+import PdfProcessor from './components/PdfProcessor.vue'
+</script>
